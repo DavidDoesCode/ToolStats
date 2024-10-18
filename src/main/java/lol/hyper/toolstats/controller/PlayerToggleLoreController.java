c5f32df0bcae631dab97041ff5872ff54ac9be1a
David <david@conceptpc.ca>
2024-10-17 22:44:02 -0500

    Add PlayerToggleLoreController

---
+++ src/main/java/lol/hyper/toolstats/controller/PlayerToggleLoreController.java
@@ -0,0 +1,36 @@
+package lol.hyper.toolstats.controller;
+
+import org.bukkit.entity.Player;
+
+import java.util.HashMap;
+
+public class PlayerToggleLoreController {
+
+    private PlayerToggleLoreController instance;
+
+    private HashMap<String, Boolean> playerAddLoreConfigs = new HashMap<>();
+
+    public PlayerToggleLoreController getInstance() {
+        if (instance == null)
+            instance = new PlayerToggleLoreController();
+
+        return instance;
+    }
+
+    public void togglePlayer(Player player) {
+        if(!playerAddLoreConfigs.containsKey(player.getName()) ||
+                (playerAddLoreConfigs.get(player.getName())))
+            playerAddLoreConfigs.put(player.getName(), false);
+        else
+            playerAddLoreConfigs.put(player.getName(), true);
+    }
+
+    public Boolean isPlayerAddLoreEnabled(Player player) {
+        if(!playerAddLoreConfigs.containsKey(player.getName()) ||
+                playerAddLoreConfigs.get(player.getName()))
+            return true;
+        else
+            return false;
+    }
+
+}
